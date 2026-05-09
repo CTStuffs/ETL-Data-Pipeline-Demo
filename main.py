9366b127b0997fb8892d70f436ae0df740285ebf
CTStuffs <ctang.work1025@gmail.com>
2026-05-09 13:33:54 +1000

    created csv cleaning notebook

---
+++ main.py
@@ -0,0 +1,7 @@
+# from src.extract import extract_csv
+# from src.transform import clean_data
+# from src.load import load_to_bigquery
+
+# df = extract_csv("data/raw/weather.csv")
+# df = clean_data(df)
+# load_to_bigquery(df)
