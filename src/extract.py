0b8aeac7151d4a7f16343c4e12635c3740a42990
Christopher Tang <ctang.work1025@gmail.com>
2026-05-13 11:16:07 +1000

    polished test_extract tests

--- src/extract.py
+++ src/extract.py
@@ -1,28 +1,31 @@
 import os
 import sys
-import csv
 
 import pandas as pd
 
 def extract_csv(file_path):
     print (f"Extracting data from {file_path}...")
     
-    if os.path.getsize(file_path) < 1:
-        print(f"Error: File {file_path} is empty.")
-        sys.exit("Exiting due to empty file...")
+    # if os.path.getsize(file_path) < 1:
+    #     print(f"Error: File {file_path} is empty.")
+    #     sys.exit("Exiting due to empty file path...")
    
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File does not exist: {file_path}")
+
+
     try:
         df = pd.read_csv(file_path)
-    except FileNotFoundError:
-        print(f"Error: File {file_path} not found.")
-        sys.exit("Exiting due to file not found...")
+    # except FileNotFoundError:
+    #     print(f"Error: File {file_path} not found.")
+    #     sys.exit("Exiting due to file not found...")
     except ValueError:
-        print(f"Error: Invalid CSV format in file {file_path}. The file may be empty or is incorrectly formatted.")
-        sys.exit("Exiting due to invalid CSV format or empty file..")
-    except csv.Error:
-        print(f"Error: CSV parsing error in file {file_path}. The file may be malformed.")
-        sys.exit("Exiting due to CSV parsing error...")
+        # print(f"Error: Invalid CSV format in file {file_path}. The file may be empty or is incorrectly formatted.")
+        # sys.exit("Exiting due to invalid CSV format or empty file...")
+        raise ValueError(f"Invalid CSV format in file {file_path}")
     except Exception as e:
-        print(f"Error occurred while extracting data from {file_path}: {e}")
-        sys.exit("Exiting due to extraction error...")
+        # print(f"Error occurred while extracting data from {file_path}: {e}")
+        # sys.exit("Exiting due to extraction error...")
+        raise Exception(f"Error occurred while extracting data from {file_path}: {e}")
+    
     return df
--- test/test_extract.py
+++ test/test_extract.py
@@ -4,35 +4,30 @@ from src.extract import extract_csv
 
 
 def test_extract_csv_valid_file():
-    result =  extract_csv("valid_file.csv")
+    result =  extract_csv("test/data/valid_data.csv")
     assert isinstance(result, pd.DataFrame)
 
-def test_extract_empty_file_path():
-    """Test handling of empty file paths"""
-    with pytest.raises(SystemExit) as exc_info:
-        extract_csv("")
+# def test_extract_empty_file_path():
+#     """Test handling of empty file paths"""
+#     with pytest.raises(SystemExit) as exc_info:
+#         extract_csv("")
     
-    assert exc_info.value.code == "Exiting due to empty file..."
+#     assert exc_info.value.code == "Exiting due to empty file path..."
 
 
 def test_extract_csv_file_not_found():
     """Test handling of non-existent file paths"""
-    with pytest.raises(SystemExit) as exc_info:
-        extract_csv("non_existent_file.csv")
+    with pytest.raises(FileNotFoundError, match=r"File does not exist: .*"):
+        extract_csv("test/data/non_existent_file.csv")
     
-    assert exc_info.value.code == "Exiting due to file not found..."
-
 
 def test_extract_csv_empty_file():
     """Test behavior when CSV has no data rows"""
-    with pytest.raises(SystemExit) as exc_info:
-        extract_csv("empty_file.csv")
+    with pytest.raises(ValueError, match = r"Invalid CSV format in file.*"):
+        extract_csv("test/data/empty_file.csv")
     
-    assert exc_info.value.code == "Exiting due to invalid CSV format or empty file.."
 
 def test_extract_bad_format_file():
     """Test behavior when CSV has no data rows"""
-    with pytest.raises(SystemExit) as exc_info:
-        extract_csv("badformatfile.csv")
-    
-    assert exc_info.value.code == "Exiting due to CSV parsing error..."    
+    with pytest.raises(ValueError, match=r"Invalid CSV format in file.*"):
+        extract_csv("test/data/badformatfile.csv")  
